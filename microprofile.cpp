4ba5535f4f02d54360c48d883af2479795e94af5
Dmytro Vovk <dmytro.vovk@fatshark.se>
2019-03-06 17:17:19 +0100

    Update microprofile.cpp
    Ooops, fixed accidentaly removed code

--- microprofile.cpp
+++ microprofile.cpp
@@ -1492,6 +1492,7 @@ void MicroProfileShutdown()
 		}
 		MicroProfileGpuShutdown();
 		MicroProfileHashTableDestroy(&S.Strings.HashTable);
+		MicroProfileStringsDestroy(&S.Strings);
 		MICROPROFILE_FREE_NON_ALIGNED(S.WSBuf.pBufferAllocation);
 
 		for (uint32_t i = 0; i < S.nNumLogs; ++i) {
