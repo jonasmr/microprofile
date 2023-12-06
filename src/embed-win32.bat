0a81c4d8ee87ecb1bcfab14bcb4616f3fef910cb
Jonas Meyer <jonas.meyer@gmail.com>
2023-12-06 10:58:51 +0100

    use new draw by default

---
+++ src/embed-win32.bat
@@ -0,0 +1,3 @@
+del ..\microprofile_html.h
+microprofile-win32-embed ..\microprofile_html.h microprofile.html ____embed____ g_MicroProfileHtml MICROPROFILE_EMBED_HTML
+microprofile-win32-embed ..\microprofile_html.h microprofilelive.html ____embed____ g_MicroProfileHtmlLive MICROPROFILE_EMBED_HTML
